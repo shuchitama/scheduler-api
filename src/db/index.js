65c87b0679619a8e7c4edcd785f4a5f32038cf50
Shuchita <shuchita@live.com>
2021-05-25 15:42:01 -0700

    add ssl

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,13 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+	connectionString: process.env.DATABASE_URL || "",
+	ssl: true,
+	dialectOptions: { ssl: { require: true } },
 });
 
 client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+	.connect()
+	.catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
